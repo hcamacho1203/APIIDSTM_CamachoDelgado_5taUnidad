7b204687a88c7ee223392fc2f28ed8b9f4625eea
hcamacho1203 <134739953+hcamacho1203@users.noreply.github.com>
2023-11-02 19:22:31 -0700

    implementación de rutas de saludo y vista para saludo

---
+++ resources/views/home_work.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        Hola
+        <?=$nombre?> <?=$apellido?>
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla odit, quis magnam placeat facilis ipsa accusamus
+        aspernatur, quibusdam dicta pariatur sit est! Nostrum expedita maxime quam nulla autem minima quisquam.
+    </p>
+    <h3>sigues aqui,
+        <?=$nombre?>?, bueno, te voy a enseñar la biblia en japones
+    </h3>
+    <p>Hajimari ni, Kami wa ten to chi to o souzou sa reta.
+    </p> Soshite, chi wa katachi o motanakatta, mushibameta. Yami ga fukumatta mizu no soko o ōteki ni shi, Kami no
+    kassei wa mizu no ue o ugokashite ita.
+    <p>Kami wa iimashita: "Hikari ga aru you ni." Soshite, hikari ga dekite.
+    </p>Sono ato, Kami wa hikari ga ii koto de aru koto o miru to, hikari o yami kara bunri hajimeta.
+    <p>Kami wa hikari o "Hi" to yobimashita, soshite yami o "Yoru" to yobimashita. Yoru ga kite, asa ga kite, kore ga
+        ichinichi no hajimari deshita.
+    </p>Soshite Kami wa iimashita: "Mizu no naka ni kūkanshin o tsukurinasai, mizu to mizu no aida ni bunri o
+    motarashinasai."
+    <p>Kami wa kūkanshin o tsukuri, kūkanshin no shita no mizu o kūkanshin no ue no mizu kara bunri shita. Kore ga
+        okotta koto deshita.
+    </p> Kami wa kūkanshin o "Sora" to yobimashita. Yoru ga kite, asa ga kite, kore ga futsuka no hajimari deshita.
+
+    <h2>sigues aqui,
+        <?=$nombre?>?, mejor deja de leer
+    </h2>
+    <p>Soshite Kami wa iimashita: "Sora no shita no mizu o hitotsu no basho ni atsumete, kawakaranu yō ni jimen ga
+        arawarinasai." Soshite kore ga okotta.
+    </p>Kami wa jimen o "Tsuchi" to yobi, mizu no sōgō o "Umi" to yobimashita. Kami wa kore ga ii koto de aru koto o
+    miru.
+    <p>Soshite Kami wa iimashita: "Jimen wa kusa, tane no tsuita shokubutsu, kajitsu o gaikata ni yotte, kajitsu to tane
+        o motsu jimen o shusshou saseinasai." Soshite kore ga okotta.
+    </p>Jimen wa kusa, tane no tsuita shokubutsu, kajitsu to tane o motsu ki, minna sorezore no gaikata ni yotte,
+    shusshou saseta. Kami wa kore ga ii koto de aru koto o miru.
+    <p>Yoru ga kite, asa ga kite, kore ga mikka no hajimari deshita.
+    </p>
+    <p>Soshite Kami wa iimashita: "Sora no kūkanshin ni akarui mono o motenasai, hi to yoru o bunri suru tame no
+        shirushi to nari, kisetsu, hi, toshi o shimeshite motarasu."
+    </p>
+    <p>Kare wa sora no kūkanshin ni akarui mono o motenagara, jimen o terasutameni. Soshite kore ga okotta.
+    </p>
+    <h1>si que eres insistente
+        <?=$nombre?>!
+    </h1>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/greeting/{nombre?}/{apellido?}', function (string $nombre = 'johnn', ?string $apellido = "X") {
+    return view('home_work', ['nombre' => $nombre, 'apellido' => $apellido]);
+})->where(['nombre' => '[a-z]+', 'apellido' => '[a-z]+']);
+
+Route::get('/saludo/{nombre}/{apellido?}', function (string $nombre, ?string $apellido = "X") {
+    return 'Hola ' . $nombre . ' ' . $apellido;
+})->where(['nombre' => '[a-z]+', 'apellido' => '[a-z]+']);
+
+//se ingresa operacion/n1/n2/suma (operacion elegida)
+Route::get('/operacion/{n1}/{n2}/{operacion}', function ($n1, $n2, $operacion) {
+    if ($operacion === 'suma') {
+        return $n1 + $n2;
+    } elseif ($operacion === 'resta') {
+        return $n1 - $n2;
+    } elseif ($operacion === 'multiplicacion') {
+        return $n1 * $n2;
+    } elseif ($operacion === 'division') {
+        if ($n2 == 0) {
+            return 'No se puede dividir por cero';
+        } else {
+            return $n1 / $n2;
+        }
+    } else {
+        return 'Operación no válida. Las operaciones válidas son: suma, resta, multiplicacion, division.';
+    }
+})->where(['n1' => '[0-9]+', 'n2' => '[0-9]+', 'operacion' => 'suma|resta|multiplicacion|division']);
